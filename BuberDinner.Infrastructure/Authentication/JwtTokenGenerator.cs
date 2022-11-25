0a9af0a4d2555da725c1ca947cce4fb0c4e6d32d
Marcin Owerczuk <muzieh@gmail.com>
2022-11-25 09:56:59 +0100

    IOption for JwtSettings

--- BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Text;
 using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Common.Interfaces.Services;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
@@ -11,16 +12,18 @@ namespace BuberDinner.Infrastructure.Authentication;
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly JwtSettings _settings;
 
-    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> settings)
     {
         _dateTimeProvider = dateTimeProvider;
+        _settings = settings.Value;
     }
 
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
         var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes("super-secret-key")), SecurityAlgorithms.HmacSha256);
+            Encoding.UTF8.GetBytes(_settings.Secret)), SecurityAlgorithms.HmacSha256);
             
             
         var claims = new[]
@@ -32,10 +35,11 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         };
 
         var securityToken = new JwtSecurityToken(
-            issuer: "BuberDinner",
-            expires: _dateTimeProvider.Now.AddDays(1).DateTime, 
+            issuer: _settings.Issuer,
+            expires: _dateTimeProvider.Now.AddMinutes(_settings.ExpiryMinutes).DateTime, 
             claims: claims,
-            signingCredentials: signingCredentials);
+            signingCredentials: signingCredentials,
+            audience: _settings.Audience);
         
         return new JwtSecurityTokenHandler().WriteToken(securityToken);
         
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -2,14 +2,16 @@ using BuberDinner.Application.Common.Interfaces.Authentication;
 using BuberDinner.Application.Common.Interfaces.Services;
 using BuberDinner.Infrastructure.Authentication;
 using BuberDinner.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.JwtSection));
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddScoped<IDateTimeProvider, DateTimeProvider>();
         return services;
